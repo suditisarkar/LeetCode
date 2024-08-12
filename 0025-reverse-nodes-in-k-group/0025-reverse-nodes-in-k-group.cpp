739d5402734bded3ee930b1db128b13bfe4e0a62
suditisarkar <122961343+suditisarkar@users.noreply.github.com>
2024-08-12 09:52:38 +0530

    Time: 12 ms (35.18%), Space: 16.4 MB (43.05%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,33 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (head == nullptr)
-            return nullptr;
-
-        ListNode* tail = head;
-
-        for (int i = 0; i < k; ++i) {
-            // There are less than k nodes in the list, do nothing.
-            if (tail == nullptr)
-                return head;
-            tail = tail->next;
+        if (head == nullptr) {
+            return head;
         }
 
-        ListNode* newHead = reverse(head, tail);
-        head->next = reverseKGroup(tail, k);
-        return newHead;
-    }
-
-    // Reverses [head, tail).
-    ListNode* reverse(ListNode* head, ListNode* tail) {
-        ListNode* prev = nullptr;
         ListNode* curr = head;
-        while (curr != tail) {
-            ListNode* next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
+        ListNode* dummy = nullptr;
+        ListNode* prev = nullptr;
+        int count = 0;
+
+        // Check if there are at least k nodes to reverse
+        while (curr != nullptr && count < k) {
+            curr = curr->next;
+            count++;
         }
+
+        curr = head;
+        if (count == k) {
+            count = 0;
+            // Reverse k nodes
+            while (curr != nullptr && count < k) {
+                dummy = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = dummy;
+                count++;
+            }
+            // Recursively call the function for the next part of the list
+            if (dummy != nullptr) {
+                head->next = reverseKGroup(dummy, k);
+            }
+        } else {
+            // If there are fewer than k nodes left, no need to reverse
+            prev = head;
+        }
+
         return prev;
     }
 };
